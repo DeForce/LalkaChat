8f8843f997da8d4a3a249cddd6cf3cea580966f3
Vladislav Ivanov <vlad@czt.lv>
2016-10-03 21:50:00 +0300

    LC-44 c2b module, fix for twitch emotes

--- modules/messaging/c2b.py
+++ modules/messaging/c2b.py
@@ -1,9 +1,32 @@
 # This Python file uses the following encoding: utf-8
 # -*- coding: utf-8 -*-
+import logging
 import os
 import random
 import re
 from modules.helpers.parser import FlagConfigParser
+
+log = logging.getLogger('c2b')
+
+
+def twitch_replace_indexes(filter_name, text, filter_size, replace_size, emotes_list):
+    emotes = []
+    message_index_list = [m.start() for m in re.finditer(filter_name, text)]
+
+    offset = replace_size - filter_size
+    for emote_data in emotes_list:
+        emote = {'emote_id': emote_data['emote_id'],
+                 'emote_pos': []}
+        for emote_pos in emote_data['emote_pos']:
+            e_start, e_end = emote_pos.split('-')
+            mod_offset = 0
+            for index, item in enumerate(message_index_list):
+                if item < int(e_start):
+                    mod_offset = offset * (index + 1)
+            emote['emote_pos'].append('{0}-{1}'.format(int(e_start) + mod_offset,
+                                                       int(e_end) + mod_offset))
+        emotes.append(emote)
+    return emotes
 
 
 class c2b:
@@ -31,12 +54,14 @@ class c2b:
     def get_message(self, message, queue):
         # Replacing the message if needed.
         # Please do the needful
-        if message is None:
-            return
-        else:
+        if message:
             for replace in self.f_items:
-                match = re.search(u'({0})'.format(replace['filter']), message['text'])
-                if match:
-                    message['text'] = message['text'].replace(replace['filter'],
-                                                              random.choice(replace['replace']))
+                if replace['filter'] in message['text']:
+                    replace_word = random.choice(replace['replace'])
+                    # Fix twitch emoticons if any
+                    if message['source'] == 'tw':
+                        message['emotes'] = twitch_replace_indexes(replace['filter'], message['text'],
+                                                                   len(replace['filter']), len(replace_word),
+                                                                   message.get('emotes', []))
+                    message['text'] = message['text'].replace(replace['filter'], replace_word)
             return message
