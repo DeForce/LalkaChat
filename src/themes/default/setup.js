efd091bdbd7cd3c99b63fb2dbd3db8bd77aef563
Ilya Chursin <ilya.chursin@dataart.com>
2017-03-27 22:33:56 +0300

    LC-204 0.3.6 js stuff
    refactor results dir name

--- src/themes/default/setup.js
+++ src/themes/default/setup.js
@@ -1,7 +1,7 @@
 const reporters = require('jasmine-reporters');
 const path = require('path');
 
-const savePath = path.resolve(__dirname, '..', '..', '..', 'result', 'javascript-tests');
+const savePath = path.resolve(__dirname, '..', '..', '..', 'results', 'javascript-tests');
 
 const reporter = new reporters.JUnitXmlReporter({ savePath: savePath, consolidateAll: false, filePrefix: 'javascript-' });
 
